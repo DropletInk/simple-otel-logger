7b34f3c26f1ca731f95aaaa82263c0f6a5e35364
soumik.dhak@droplet.ink <soumik.dhak@droplet.ink>
2026-03-19 16:25:28 +0530

    add exporter

--- src/auto.ts
+++ src/auto.ts
@@ -3,8 +3,8 @@ import { initTelemetry } from "./telemetry.js"
 const config = {
   serviceName: process.env.OTEL_SERVICE_NAME ||"unknown service",
   environment: process.env.NODE_ENV || "development",
-  traceexporterUrl: process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
-  //metricexporterUrl: process.env.OTEL_EXPORTER_OTLP_METRICS_ENDPOINT,
+  exporterUrl: process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || "",
+  metricsExporterUrl: process.env.OTEL_EXPORTER_OTLP_METRICS_ENDPOINT || "",
 }
 
 if (config.serviceName) {
